2e8fc963e4fb5d62ad7ab13db455dc2110d27bcc
Samuel Resendez <saresend@usc.edu>
2016-11-19 11:55:41 -0800

    init commit

---
+++ OnCall(Manager)/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OnCall(Manager)
+//
+//  Created by Samuel Resendez on 11/19/16.
+//  Copyright © 2016 Samuel Resendez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
